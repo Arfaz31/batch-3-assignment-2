39847128cd95c5bdc50465d802b9531c6ca8c9fb
Arfaz31 <arfazahamed31@gmail.com>
2024-05-22 19:01:49 +0600

    roue-not-found-middleware

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,13 @@ app.use(cors());
 app.use("/api", productRouters);
 app.use("/api/orders", orderRouter);
 
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
